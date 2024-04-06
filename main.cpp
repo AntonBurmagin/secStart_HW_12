59547ab2f06a2bbb20dd53fe26b0789558410399
Anton Burmagin <burmagin.anton.s@gmail.com>
2024-04-06 22:58:07 +0300

    third commit: solve 4th task

--- main.cpp
+++ main.cpp
@@ -22,29 +22,43 @@ int main() {
     */
 
    //lesson 12.5.2
-    std::cout << "Input 10 float numbers:" << std::endl;
-    float arr[10];
-    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
-        std::cin >> arr[i];
-    }
+    // std::cout << "Input 10 float numbers:" << std::endl;
+    // float arr[10];
+    // for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+    //     std::cin >> arr[i];
+    // }
     
-    for (int i = sizeof(arr) / sizeof(arr[0]) - 1; i > 0; i--) {
-        int minIndex = i;
-        for (int k = 0; k < i; k++) {
-            if (arr[k] < arr[minIndex]) {
-                minIndex = k;
-            }
-        }
-        if (arr[i] > arr[minIndex]) {
-            std::swap(arr[i], arr[minIndex]);
-        }
-    }
-    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
+    // for (int i = sizeof(arr) / sizeof(arr[0]) - 1; i > 0; i--) {
+    //     int minIndex = i;
+    //     for (int k = 0; k < i; k++) {
+    //         if (arr[k] < arr[minIndex]) {
+    //             minIndex = k;
+    //         }
+    //     }
+    //     if (arr[i] > arr[minIndex]) {
+    //         std::swap(arr[i], arr[minIndex]);
+    //     }
+    // }
+    // for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+    //     std::cout << arr[i] << " ";
+    // }
+    // std::cout << std::endl;
     // 1.2 -1 15.8 9.4 8.1 -7.3 6.4 18 10 11.155
     
+   //lesson 12.5.3
+
+
+   //lesson 12.5.4 
+   int numbers[15] = {114, 111, 106, 107, 108, 105, 115, 108, 110, 109, 112, 113, 116, 117, 118};
+   int min = numbers[0];
+   int repeatNumber;
+   int sum = 0;
+   for (int i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++) {
+        sum += numbers[i];
+        if (min > numbers[i]) min = numbers[i];
+   }
+   repeatNumber = sum - min * 14 - 13 * 7;
+   std::cout << "X (starting number): " << min << ", repeating number: " << repeatNumber << std::endl;
 
 
 }
